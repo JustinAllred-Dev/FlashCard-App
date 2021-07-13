cbb39f6710c2208f7177d4e039d689f4cd22e2d1
Justin Allred <justinallred5@gmail.com>
2021-07-13 15:40:22 -0500

    fixed routing issue with editcard edit

--- src/Card/CardForm.js
+++ src/Card/CardForm.js
@@ -1,39 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function CardForm({ card, handleChange, handleSubmit, onFinished, deckId }) {
+function CardForm({ card, handleChange, handleSubmit, deckId }) {
   return (
     <form onSubmit={handleSubmit}>
       <fieldset>
         <div className="form-group">
-          <label htmlFor="front">
-            Front:{" "}
-            <textarea
-              id="front"
-              className="form-control"
-              name="front"
-              type="text"
-              placeholder="Front of card:"
-              rows="2"
-              value={card.front}
-              onChange={handleChange}
-            />
-          </label>
+          <label htmlFor="front">Front: </label>
+          <textarea
+            id="front"
+            name="front"
+            tabIndex="1"
+            className="form-control"
+            required={true}
+            placeholder="Front side of card"
+            value={card.front}
+            onChange={handleChange}
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="back">
-            Back:{" "}
-            <textarea
-              id="back"
-              className="form-control"
-              name="back"
-              type="text"
-              placeholder="Back of card:"
-              rows="3"
-              value={card.back}
-              onChange={handleChange}
-            />
-          </label>
+          <label htmlFor="back">Back: </label>
+          <textarea
+            id="back"
+            name="back"
+            tabIndex="2"
+            className="form-control"
+            required={true}
+            placeholder="Back side of card"
+            value={card.back}
+            onChange={handleChange}
+          />
         </div>
         <Link className="btn btn-secondary mr-2" to={`/decks/${deckId}`}>
           Cancel
